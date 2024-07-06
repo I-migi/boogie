f2388e90726e5be2095d4bf9abd9eab26e7fdca7
sunwoo <sunn1017@naver.com>
2024-07-06 16:10:10 +0900

    first commit

---
+++ src/main/java/team3/boogie/BoogieApplication.java
@@ -0,0 +1,13 @@
+package team3.boogie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoogieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoogieApplication.class, args);
+	}
+
+}

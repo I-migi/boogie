52c3ed9c7ae802e8fc0dd4d0e1e1f6af1ca9b4e3
sunwoo <sunn1017@naver.com>
2024-07-08 13:37:47 +0900

    second commit

---
+++ src/test/java/team3/boogie/BoogieApplicationTests.java
@@ -0,0 +1,13 @@
+package team3.boogie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoogieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c3b5dcdc4aba43fedbaa16c1b0862136fe6ec8e9
I-migi <sunn10189@gmail.com>
2024-07-25 17:36:08 +0900

    동기화

--- src/main/java/team3/boogie/BoogieMysqlApplication.java
+++ src/main/java/team3/boogie/BoogieMysqlApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoogieApplication {
+public class BoogieMysqlApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BoogieApplication.class, args);
+        SpringApplication.run(BoogieMysqlApplication.class, args);
     }
 
 }
